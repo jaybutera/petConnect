0c977976f6ed81f92766c8f700a39ea9492da177
Jay Butera <buterajay@gmail.com>
2017-09-22 22:27:28 -0500

    Moar

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-const express = require('express');
-const app = express();
+const express  = require('express');
+const mongoose = require('mongoose');
+const app      = express();
+
+/**************************
+ *     MONGODB SETUP     *
+**************************/
+mongoose.connect('mongodb://localhost/test', { useMongoClient: true, promiseLibrary: global.Promise });
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+   console.log("hells ya");
+});
+
+// Pet Schema
+const pet = mongoose.Schema({
+   url: String,
+   img: { data: Buffer, contentType: String },
+   descriptor: [],
+});
+
+/**************************
+ *     START SERVER      *
+**************************/
 
 app.listen(3000, () => {
    console.log("listening on port 3000");
