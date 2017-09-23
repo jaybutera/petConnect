9173d233aa7e075213b7344c5016f4a44fd35ab3
Jay Butera <buterajay@gmail.com>
2017-09-23 12:44:05 -0500

    Post endpoint

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
 const express  = require('express');
 const mongoose = require('mongoose');
+const multer   = require('multer')
+const upload   = multer({ dest: 'uploads/' })
 const app      = express();
 
 /**************************
  *     MONGODB SETUP     *
 **************************/
-mongoose.connect('mongodb://localhost/test', { useMongoClient: true, promiseLibrary: global.Promise });
+mongoose.connect('mongodb://localhost/pets', { useMongoClient: true, promiseLibrary: global.Promise });
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+db.once('open', () => {
    console.log("hells ya");
 });
 
 // Pet Schema
 const pet = mongoose.Schema({
    url: String,
-   img: { data: Buffer, contentType: String },
-   descriptor: [],
+   img_url: String,
+   descriptors: [String],
 });
+
 
 /**************************
  *     START SERVER      *
 **************************/
+
+app.post('/search', upload.single('user_pet'), (req, res, next) => {
+   console.log('elo');
+   console.log( req.file );
+
+   res.send({});
+});
 
 app.listen(3000, () => {
    console.log("listening on port 3000");
